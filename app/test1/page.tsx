f7d2df4e73e7205672c0fdc1720746baeb5e7908
Reetika-Chavan <reetika.chavan@contentstack.com>
2025-11-04 12:08:34 +0530

    initial commit

---
+++ app/test1/page.tsx
@@ -0,0 +1,14 @@
+export default function Test1() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-col items-center gap-6 text-center px-8">
+        <h1 className="text-4xl font-semibold text-black dark:text-zinc-50">
+          Test Page 1
+        </h1>
+        <p className="text-lg text-zinc-600 dark:text-zinc-400">
+          Simple test page for cookie testing
+        </p>
+      </main>
+    </div>
+  );
+}
